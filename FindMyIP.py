64ce86dbe41d68aadd7b46167244fd7efff805fc
Mehran Seifalinia <70560051+Mehran-Seifalinia@users.noreply.github.com>
2023-08-02 16:01:31 +0330

    Fix minor bugs
    - Add comments
    - Define dynamic values to variables to be more easy to handle it
    - Fix "You're not connected" error, when you are connected
    - Add timeout

--- FindMyIP.py
+++ FindMyIP.py
@@ -1,29 +1,45 @@
-from socket import socket, AF_INET, SOCK_DGRAM
+from socket import socket, AF_INET, SOCK_STREAM, SOCK_DGRAM, setdefaulttimeout, error
 from urllib.request import urlopen
 
+# Check if internet connection is available by attempting to connect to www.google.com on port 80.
 def internet():
-    url = "https://www.google.com/"
+    host = "www.google.com"
+    port = 80
+    timeout = 5  # 5 seconds
+
     try:
-        urlopen(url)
+        setdefaulttimeout(timeout)
+        socket(AF_INET, SOCK_STREAM).connect((host, port))
         return True
-    except:
+    except error:
         return False
 
+# Get the local IP address by connecting to Google's public DNS server (8.8.8.8) on port 80.
 def internal():
+    google_dns = "8.8.8.8"
+    port = 80
+
     try:
         connection = socket(AF_INET, SOCK_DGRAM)
-        connection.connect(("8.8.8.8", 80))
+        connection.connect((google_dns, port))
         local_ip_address = connection.getsockname()[0]
-        connection.close()
         return local_ip_address
     except Exception as e:
         print(e)
-        exit()
+        return None
+    finally:
+        connection.close()
 
+# Get the external (public) IP address by fetching it from the ipify.org API.
 def external():
+    api_url = "https://api.ipify.org/"
+    decode_type = "UTF-8"
+
     try:
         if internet():
-            external_ip_address = urlopen("https://api.ipify.org/").read().decode("UTF-8")
+            response = urlopen(api_url)
+            external_ip_address = response.read().decode(decode_type)
+            response.close()
             return external_ip_address
         else:
             print("You are not connected to the internet!\nPlase check your connection.")
